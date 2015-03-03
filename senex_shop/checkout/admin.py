c005319addeb15f8b5c127331cbb50d35b108d5d
Michael Anderson <manderson@safaribooksonline.com>
2015-03-02 21:37:57 -0700

    Convert fields to raw_id fields to prevent selection boxes

--- senex_shop/checkout/admin.py
+++ senex_shop/checkout/admin.py
@@ -4,7 +4,7 @@ from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('number', 'email', 'date_ordered', )
-    search_fields = ('number', )
+    raw_id_fields = ('user', 'cart', 'billing_address', 'shipping_address', 'charge', )
 
 
 admin.site.register(Order, OrderAdmin)
